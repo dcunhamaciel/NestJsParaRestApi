d24c130b7b63bb210b9885c32d7422b6f117d44c
dcunhamaciel <dcunhamaciel@gmail.com>
2026-06-20 10:24:24 -0300

    Decorator @Post para o método http POST

--- 02. Crud/src/recados/recados.controller.ts
+++ 02. Crud/src/recados/recados.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
   @Get(':id')
   findOne(@Param('id') id: number): string {
-    return `Essa rota retorna o recado ID ${id}`;
+    return `Essa rota retorna o recado ID ${id}!`;
   }
 
   @Get()
   findAll(): string {
     return 'Essa rota retorna todos os recados!';
+  }
+
+  @Post()
+  create() {
+    return `Essa rota cria um recado!`;
   }
 }
