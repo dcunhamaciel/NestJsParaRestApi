28f29bec2f43e838fde69e317f63044781a964a3
dcunhamaciel <dcunhamaciel@gmail.com>
2026-06-21 10:38:01 -0300

    Segurança com whitelist e forbidNonWhitelisted

--- 02. Crud/src/main.ts
+++ 02. Crud/src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
