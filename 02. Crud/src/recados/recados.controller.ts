30218151d3befe30add958fac5a0b160a9005e50
dcunhamaciel <dcunhamaciel@gmail.com>
2026-06-23 21:32:03 -0300

    Interceptors - criando um cache simples

--- 02. Crud/src/recados/recados.controller.ts
+++ 02. Crud/src/recados/recados.controller.ts
@@ -20,9 +20,11 @@ import { ParseIntIdPipe } from 'src/common/pipes/parse-int-id.pipe';
 import { AddHeaderInterceptor } from 'src/common/interceptors/add-header.interceptor';
 import { TimingConnectionInterceptor } from 'src/common/interceptors/timing-connection.interceptor';
 import { ErrorHandlingInterceptor } from 'src/common/interceptors/error-handling.interceptor';
+import { SimpleCacheInterceptor } from 'src/common/interceptors/simple-cache.interceptor';
 
 @Controller('recados')
 @UsePipes(ParseIntIdPipe)
+@UseInterceptors(SimpleCacheInterceptor)
 export class RecadosController {
   constructor(private readonly recadosService: RecadosService) {}
 
