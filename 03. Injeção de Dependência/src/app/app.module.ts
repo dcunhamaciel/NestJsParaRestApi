4e556041b9ae76815b7c458727b995ef8d8f767a
Diego Maciel <diego.maciel@diretriz.net>
2026-06-24 17:40:19 -0300

    Estrutura inicial do projeto

--- 03. Injeção de Dependência/src/app/app.module.ts
+++ 03. Injeção de Dependência/src/app/app.module.ts
@@ -1,15 +1,9 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
 import { RecadosModule } from 'src/recados/recados.module';
-import { SimpleMiddleware } from 'src/common/middlewares/simple.middleware';
 import { APP_FILTER } from '@nestjs/core';
 import { MyExceptionFilter } from 'src/common/filters/my-exception.filter';
 
@@ -37,11 +31,4 @@ import { MyExceptionFilter } from 'src/common/filters/my-exception.filter';
     },
   ],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(SimpleMiddleware).forRoutes({
-      path: '*',
-      method: RequestMethod.ALL,
-    });
-  }
-}
+export class AppModule {}
